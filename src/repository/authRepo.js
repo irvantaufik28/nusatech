24187a22766d1e126947e10870924cf3fc5d764e
irvan taufik <irvantaufik28@gmail.com>
2022-12-16 10:39:36 +0700

    setup auth repo

---
+++ src/repository/authRepo.js
@@ -0,0 +1,23 @@
+const { User } = require('../models')
+
+class AuthRepo {
+    constructor () {
+        this.UserModel = User;
+    }
+
+    async login (email) {
+        const user = await this.UserModel.findOne({
+            where: {
+                email
+            }
+        })
+        return user;
+    }
+
+    async register(userData) {
+        const user = await this.UserModel.create(userData)
+        return user;
+    }
+}
+
+module.exports = AuthRepo;
