08d297fb4d830593fb19324a012a9879322b433c
irvan taufik <irvantaufik28@gmail.com>
2022-12-16 10:09:01 +0700

    setup database

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const app = require('./app')
 
 const port = process.env.PORT;
-const host = process.env.HOSTl;
+const host = process.env.HOST;
 
 app.listen(port, host, ()=> {
     console.log(`server listening on http://${host}:${port}`)
