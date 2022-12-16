6df76fe64a47daca6ad04d9e66cd06df4d6f6ec4
irvan taufik <irvantaufik28@gmail.com>
2022-12-16 11:33:53 +0700

    add seeder user

---
+++ src/database/seeders/20221216043237-user.js
@@ -0,0 +1,23 @@
+const bcrypt = require("bcrypt")
+'use strict'
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.bulkInsert(
+      'Users',
+      [
+        {
+          email:'irvan@mail.com',
+          password: bcrypt.hashSync('password', 10),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ],
+      {},
+    )
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('Users', null, {})
+  },
+}
